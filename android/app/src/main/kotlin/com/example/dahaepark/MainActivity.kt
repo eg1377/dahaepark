050488332843bc80ccc31016f65f850915cf5752
da hae park <eg1377@naver.com>
2023-03-10 16:54:22 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/dahaepark/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dahaepark
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:dahaepark/seens/home_page.dart';
+import 'package:dahaepark/seens/login_page.dart';
+import 'package:dahaepark/seens/main_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: LoginPage(),
+    );
+  }
+}
+
+
+
+/*
+
+class MyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:const Text('Appbar'),
+        centerTitle: true,
+        elevation: 0.0,
+        backgroundColor: Colors.red[200],
+        actions: [
+          IconButton(
+              onPressed: () => {print("hi22")},
+              icon:const Icon(Icons.shopping_cart)),
+          IconButton(onPressed: () => {print("hi33")}, icon: const Icon(Icons.search))
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: const Text("park"),
+              accountEmail: const Text("naver.com"),
+              currentAccountPicture: const CircleAvatar(
+                backgroundColor: Colors.white,
+                backgroundImage: AssetImage('lib/assets/image/peep-70.png'),
+              ),
+              otherAccountsPictures: const [
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  backgroundImage: AssetImage('lib/assets/image/sitting.svg'),
+
+
+                )
+              ],
+              onDetailsPressed: () => {print("clicked")},
+              decoration: BoxDecoration(
+                  color: Colors.red[200],
+                  borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(40),
+                      bottomRight: Radius.circular(40))),
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.home,
+                color: Colors.grey,
+              ),
+              title: const Text('Home'),
+              onTap: () => {print('Home')},
+              trailing: const Icon(Icons.add),
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.settings,
+                color: Colors.grey,
+              ),
+              title: const Text('Setting'),
+              onTap: () => {print('Setting')},
+              trailing: const Icon(Icons.add),
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.question_answer,
+                color: Colors.grey,
+              ),
+              title: const Text('Q&A'),
+              onTap: () => {print('Home')},
+              trailing: const Icon(Icons.add),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+*/
